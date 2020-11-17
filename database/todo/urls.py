dfd2aa56aebe35f7c8096be70e4f88e1cdba814b
Rameshtech17 <learnelectrotech@gmail.com>
2020-11-17 20:03:10 +0530

    test1

--- database/todo/urls.py
+++ database/todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import todoAPIView
+from .views import todoAPIView,todoUpdateAPIView
 
 urlpatterns = [
     path('todo/',todoAPIView.as_view()),
+    path('todo1/<int:id>',todoUpdateAPIView.as_view()),
 
 ]
--- database/todo/views.py
+++ database/todo/views.py
@@ -24,7 +24,24 @@ class todoAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        
+
+class todoUpdateAPIView(APIView):
+
+    def get_object(self, id):
+
+        try:
+            return toDoText.objects.get(id=id)
+
+        except toDoText.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+
+    def get(self, request,id):
+        qury = self.get_object(id)
+        serializer = todoSerializer(qury)
+        return Response(serializer.data)
+
+
     def put(self,request,id):
         qury = self.get_object(id)
         serializer = todoSerializer(qury,data=request.data)
@@ -37,6 +54,5 @@ class todoAPIView(APIView):
         clsses = self.get_object(id)
         clsses.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 
